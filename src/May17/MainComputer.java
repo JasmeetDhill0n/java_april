bfc63625a2a9358251e3c11e3fa27a0137ccb295
unknown <jasmeetdhillon005@gmail.com>
2025-06-29 22:56:03 -0400

    StringAndArrayAssignment

--- src/May17/MainComputer.java
+++
@@ -1,8 +0,0 @@
-package May17;
-
-public class MainComputer {
-    public static void main(String[] args) {
-        Computer computer = new Computer(34,"Lenova");
-        computer.Display();
-    }
-}
